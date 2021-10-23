a55d47399145052421ce2f0af98929470f872336
Antony Holmes <antony@antonybholmes.com>
2021-10-23 14:28:26 -0400

    Bug fixes and updates.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./public/**/*.html", "./src/**/*.{astro,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "Helvetica", "Arial"],
+      },
+      width: {
+        "1/10": "10%",
+        "2/10": "20%",
+        "3/10": "30%",
+        "4/10": "40%",
+        "5/10": "50%",
+        "6/10": "60%",
+        "7/10": "70%",
+        "8/10": "80%",
+        "9/10": "90%",
+        "3/20": "15%",
+        "9/20": "45%"
+      },
+    },
+  },
+}
